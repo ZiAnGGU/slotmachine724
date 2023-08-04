b503c906f2dc50be6ee9889eba9d6fc9cc1e3a3a
ZiAnGGU <zianggu2@gmail.com>
2023-08-04 15:14:12 -0700

    spin & reset button in js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 	/*----- constants -----*/
-
-
+const numArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+const spinBtn = document.querySelector("#spin")
+const resetBtn = document.querySelector("#reset")
 	/*----- state variables -----*/
 
 
@@ -8,6 +9,7 @@
 
 
 	/*----- event listeners -----*/
-
+spinBtn.addEventListener("click", spin)
+resetBtn.addEventListener("click", reset)
 
 	/*----- functions -----*/
