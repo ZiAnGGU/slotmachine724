6e13025ce79c0b4b50a594284a0892fbc0e06590
ZiAnGGU <zianggu2@gmail.com>
2023-08-04 16:54:45 -0700

    get random numbers function in js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 	/*----- constants -----*/
-const numArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-const spinBtn = document.querySelector("#spin")
-const resetBtn = document.querySelector("#reset")
+// const numArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+const getBoardNumBtn = document.getElementById("spin")
+const resetBtn = document.getElementById("reset")
+
+
 	/*----- state variables -----*/
 
+// document.querySelectorAll(".board").innerHTML = x;
 
 	/*----- cached elements  -----*/
 
@@ -13,3 +17,16 @@ spinBtn.addEventListener("click", spin)
 resetBtn.addEventListener("click", reset)
 
 	/*----- functions -----*/
+
+// let x = Math.floor(Math.random() * 10); // max 10 
+// getRandomNum (() => {
+//     value = 10;
+// })
+function getRandomNum () {
+    return Math.floor(Math.random() * 10);
+}
+function getBoardNum () {
+    boardone = getRandomNum();
+    boardtwo = getRandomNum();
+    boardthree = getRandomNum();
+}
