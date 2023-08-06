92ea279e2e85eaaca6b5ad6b108037c02abf4fd3
ZiAnGGU <zianggu2@gmail.com>
2023-08-06 13:31:30 -0700

    edit in css

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
 	/*----- constants -----*/
 // const numArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+
+	/*----- state variables -----*/
+let boardone, boardtwo, boardthree; 
+
+
+	/*----- cached elements  -----*/
 const getBoardNumBtn = document.getElementById("spin")
 const resetBtn = document.getElementById("reset")
 
-
-	/*----- state variables -----*/
-
-// document.querySelectorAll(".board").innerHTML = x;
-
-	/*----- cached elements  -----*/
-
-
 	/*----- event listeners -----*/
-spinBtn.addEventListener("click", spin)
+getBoardNumBtn.addEventListener("click", spin)
 resetBtn.addEventListener("click", reset)
 
 	/*----- functions -----*/
@@ -22,6 +20,8 @@ resetBtn.addEventListener("click", reset)
 // getRandomNum (() => {
 //     value = 10;
 // })
+// document.querySelectorAll(".board").innerHTML = x;
+
 function getRandomNum () {
     return Math.floor(Math.random() * 10);
 }
